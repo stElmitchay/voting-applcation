e691f87d41f4d3465f0308db3cab5cc5e78ff2b7
stElmitchay <elmitchay@gmail.com>
2025-03-26 15:41:51 +0000

    config change

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true, // 👈 this allows the build to proceed
-    },
-  };
-  
-  export default nextConfig;
-  
